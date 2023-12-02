49fbe54a3da5c96a58360d5b16b2d672311e04d4
00009199 <wiut00009199@gmail.com>
2023-12-02 15:15:08 +0500

    Update

---
+++ src/firebase.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBefAFEh1NVnt2YaSBn9KMkmKWy6ieZKV8",
+    authDomain: "iot2023-7a185.firebaseapp.com",
+    databaseURL: "https://iot2023-7a185-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "iot2023-7a185",
+    storageBucket: "iot2023-7a185.appspot.com",
+    messagingSenderId: "766170917410",
+    appId: "1:766170917410:web:acb8a3c7663131f16ab87b",
+    measurementId: "G-5H6EMVGPHY"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
